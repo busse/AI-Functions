0ba3286f9aed940b0b6a2fe86c056f57f1950301
Chris Busse <chrisbusse@gmail.com>
2023-04-09 03:03:27 -0400

    Update `test_ai_function.py` with ChatGPT `gpt-4` response for issue 9999

--- test_ai_function.py
+++ test_ai_function.py
@@ -11,15 +11,16 @@ openai.api_key = keys.OPENAI_API_KEY
 
 # Run all tests, print the results, and return the number of failed tests
 def run_tests(model):
-    test_functions = [test_1, test_2, test_3, test_4, test_5, test_6]
+    test_functions = [test_1, test_2, test_3, test_4, test_5, test_6, test_7]
     test_names = [
         "Generate fake people",
         "Generate Random Password",
         "Calculate area of triangle",
         "Calculate the nth prime number",
         "Encrypt text",
-        "Find missing numbers"
-]
+        "Find missing numbers",
+        "Generate SVG triangle with style information"
+    ]
     failed_tests = []
 
     i = 0
@@ -42,130 +43,31 @@ def run_tests(model):
     print(f"Success Rate: {len(test_functions) - len(failed_tests)}/{len(test_functions)}")
 
 # Ai function test 1
-def test_1(model):
-    function_string = "def fake_people(n: int) -> list[dict]:"
-    args = ["4"]
-    description_string = """Generates n examples of fake data representing people in valid JSON format using double quotes not single quotes, 
-            each with a name and an age."""
+# ... Leave other test functions unchanged ...
+
+# Ai function test 7
+def test_7(model):
+    function_string = "def generate_svg_triangle_with_style(style_info: str) -> str:"
+    args = ["'fill: red; stroke: black; stroke-width:1;'"]
+    description_string = """Generates a drawing of a triangle in SVG format with the given style information."""
 
     result_string = ai_functions.ai_function(function_string, args, description_string, model)
 
     print(f"Output: {result_string}")
-    # Assert the result can be parsed as is a list of dictionaries
-    print("Testing if result is a a string...")
-    assert isinstance(result_string, str)
-    result = None
+
+    # Assert the result is a valid SVG triangle with the given style information
     try:
-        print("Testing if result can be parsed as a list of dictionaries...")
-        # Parse the result as a list of dictionaries
-        result = json.loads(result_string)
-    except Exception as e:
-        # If the result can't be parsed as a list of dictionaries, the test fails
-        assert False
-    
-    # Assert the length of the result is equal to the number of people requested
-    print("Testing if the length of the result is equal to the number of people requested...")
-    if result:
-        assert len(result) == int(args[0])
-    else:
-        assert False
+        print("Testing if the output is a string...")
+        assert isinstance(result_string, str)
+        
+        # Hardcoding point values for the triangle
+        a_x, a_y, b_x, b_y, c_x, c_y = 0, 0, 100, 0, 50, 50
 
-# Ai function test 2
-def test_2(model):
-    function_string = "def random_password_generator(length: int, special_chars: bool) -> str:"
-    args = ["12", "True"]
-    description_string = """Generates a random password of given length with or without special characters."""
-
-    result_string = ai_functions.ai_function(function_string, args, description_string, model)
-
-    print(f"Output: {result_string}")
-
-    # Assert the length of the result is equal to the length requested
-    print("Testing if the length of the result is equal to the length requested...")
-    assert len(result_string) == int(args[0])
-
-# Ai function test 3
-def test_3(model):
-    function_string = "def calculate_area_of_triangle(base: float, height: float) -> float:"
-    args = ["15", "6.5"]
-    description_string = """Calculates the area of a triangle given its base and height."""
-
-    result_string = ai_functions.ai_function(function_string, args, description_string, model)
-    print(f"Output: {result_string}")
-
-    # Assert the result can be parsed as a float
-    print("Testing if result is a a float...")
-    try:
-        assert isinstance(float(result_string), float)
+        print("Testing if the output contains the appropriate SVG format and style information...")
+        assert f'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><polygon points="{a_x},{a_y} {b_x},{b_y} {c_x},{c_y}" style="{args[0].strip("'")}" /></svg>' in result_string
     except Exception as e:
         print(e)
         assert False
-
-    # Assert the result is equal to the expected area of the triangle
-    expected_area = (float(args[0]) * float(args[1])) / 2
-    print("Testing if the result is equal to the expected area of the triangle, which is: " + str(expected_area))
-    assert float(result_string) == pytest.approx(expected_area)
-
-# Ai function test 4
-def test_4(model):
-    function_string = "def get_nth_prime_number(n: int) -> int:"
-    args = ["10"]
-    description_string = """Finds and returns the nth prime number."""
-
-    result_string = ai_functions.ai_function(function_string, args, description_string, model)
-
-    print(f"Output: {result_string}")
-
-    # Assert the result can be parsed as an integer
-    print("Testing if result is a a integer...")
-    try:
-        assert isinstance(int(result_string), int)
-    except Exception as e:
-        print(e)
-        assert False
-
-    # Assert the result is equal to the expected nth prime number
-    expected_prime_number = 29
-    print("Testing if the result is equal to the expected nth prime number, which is: " + str(expected_prime_number))
-    assert int(result_string) == expected_prime_number
-
-# Ai function test 5
-def test_5(model):
-    function_string = "def encrypt_text(text: str, key: str) -> str:"
-    args = ["'Hello, World!'", "'abc123'"]
-    description_string = """Encrypts the given text using a simple character substitution based on the provided key."""
-
-    result_string = ai_functions.ai_function(function_string, args, description_string, model)
-
-    print(f"Output: {result_string}")
-
-    # Assert the result has the same length as the input text
-    print("Testing if the result has the same length as the input text...")
-    assert len(result_string) == len(args[0])
-
-# Ai function test 6
-def test_6(model):
-    function_string = "def find_missing_numbers_in_list(numbers: list[int]) -> list[int]:"
-    args = ["[3, 5, 8, 15, 16]"]
-    description_string = """Finds and returns a list of missing numbers in a given sorted list."""
-
-    result_string = ai_functions.ai_function(function_string, args, description_string, model)
-
-    print(f"Output: {result_string}")
-
-    # Assert the result can be parsed as a list
-    try:
-        result_list = ast.literal_eval(result_string)
-        print("Testing if result is a a list...")
-        assert isinstance(result_list, list)
-    except Exception as e:
-        print(e)
-        assert False
-
-    # Assert the result list contains the expected missing numbers
-    expected_missing_numbers = [4, 6, 7, 9, 10, 11, 12, 13, 14]
-    print("Testing if the result list contains the expected missing numbers...")
-    assert result_list == expected_missing_numbers
 
 run_tests("gpt-4")
 run_tests("gpt-3.5-turbo")
